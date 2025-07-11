91de82f6687b9b7465c335cee43feadaeb600814
Sarajit Mandal <bdsarajit499@gmail.com>
2025-07-11 19:15:56 +0600

    part-1 completed

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { UserService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
-
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status-codes"
 
 
 // createUser 
@@ -9,20 +10,25 @@ import { catchAsync } from "../../utils/catchAsync";
 const createUser = catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
     const user = await UserService.createUser(req.body)
 
-    res.status(200).json({
-        data:user,
-        message : "User created successfully"
-    })
+   sendResponse(res,{
+    statusCode: httpStatus.CREATED,
+    success:true,
+    message: "User Created Successfull",
+    data: user,
+   })
 })
 
 
 // getAll Users 
 const getAllusers =catchAsync(async(req:Request,res:Response)=>{
     const users = await UserService.getAllUsers()
-    res.status(200).json({
-        data : users,
-        message : "get All user successfully"
-    })
+    sendResponse(res,{
+    statusCode: httpStatus.CREATED,
+    success:true,
+    message: "All Users Retrieved Successfully",
+    meta: users.meta,
+    data: users,
+   })
    
 })
 
