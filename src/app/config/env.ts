beeb00b1318474bd6d6c89042afb2443fad4345f
sarajit mondal <bdsarajit499@gmail.com>
2025-07-13 08:04:23 +0600

    tokenGenerateToken and verifyToken completed

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,7 +6,7 @@ dotenv.config()
 
 
 const loadEnvVariables = ()=>{
-    const requiredEnvVariables = ["PORT","DB_URL","NODE_ENV","BCRYPT_SALT_ROUND"]
+    const requiredEnvVariables = ["PORT","DB_URL","NODE_ENV","BCRYPT_SALT_ROUND","JWT_ACCESS_EXPIRES","JWT_ACCESS_SECRET"]
 
     requiredEnvVariables.forEach((key)=>{
         if(!process.env[key]){
@@ -20,7 +20,10 @@ const loadEnvVariables = ()=>{
         PORT : process.env.PORT as string,
         DB_URL : process.env.DB_URL as string,
         NODE_ENV : process.env.NODE_ENV as string,
-        BCRYPT_SALT_ROUND : process.env.BCRYPT_SALT_ROUND as string
+        BCRYPT_SALT_ROUND : process.env.BCRYPT_SALT_ROUND as string,
+        JWT_ACCESS_EXPIRES : process.env.JWT_ACCESS_EXPIRES as string,
+        JWT_ACCESS_SECRET : process.env.JWT_ACCESS_SECRET  as string
+        
 
     }
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { IUser } from "../user/user.interface";
 import { User } from "../user/user.modle";
 import httpStatusCode from "http-status-codes"
 import bcrypt from "bcryptjs"
+import { generateToken } from "../../utils/jwt";
+import { envVabs } from "../../config/env";
 
 const credentialsLogin = async(payload:Partial<IUser>)=>{
 const {email,password} = payload;
@@ -19,8 +21,18 @@ if(!isPasswordMatched){
     throw new AppError(httpStatusCode.BAD_REQUEST,"Incorrect Password")
 }
 
+// jwt payload
+const jwtPayload = {
+    userId : isUserExist?._id,
+    email : isUserExist?.email,
+    role : isUserExist?.role
+}
+
+const jwtToken = generateToken(jwtPayload,envVabs.JWT_ACCESS_SECRET,envVabs.JWT_ACCESS_EXPIRES)
+// eslint-disable-next-line no-console
+console.log(jwtToken)
 return{
-    email
+   jwtToken
 }
 }
 
